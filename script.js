f2ea7cae819999e83b65e9bcca5526970f2d5f48
Mhenr1 <mhfozorio@gmail.com>
2021-12-11 21:04:41 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const calculadora = document.getElementById('calculadora');
+
+calculadora.addEventListener('click', configura);
+
+
+function configura({
+    target
+}) {
+    const display = document.getElementById('display');
+    if (display.innerText == "Infinity") {
+        display.innerText = ""
+    }
+    display.classList.remove('erro');
+    let ignora = ["display", "calculadora"];
+    if (ignora.includes(target.id)) return
+
+    const value = target.dataset.value;
+
+    if (value == "ac") {
+        display.innerText = ""
+    } else
+    if (value == "del") {
+        let text = display.innerText;
+        display.innerText = text.substring(0, text.length - 1);
+    } else
+    if (value == "=") {
+        calcula(display);
+    } else {
+        display.innerText += value
+    }
+
+}
+
+function calcula(display) {
+    try {
+        display.innerText = Function("return " + display.innerText)() || 0
+    } catch (e) {
+        if (e.name == 'SyntaxError') {
+            display.innerText = ""
+            display.classList.add('erro')
+        }
+    }
+
+}
